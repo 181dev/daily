8cb2873971174c0bf560520009af3a9201ff6ae9
181dev <181dev@users.noreply.github.com>
2014-07-09 14:44:17 +0900

    initial commit

---
+++ Daily/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace Daily
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.checkedListBoxTasks = new System.Windows.Forms.CheckedListBox();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.終了ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // checkedListBoxTasks
+            // 
+            this.checkedListBoxTasks.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.checkedListBoxTasks.CheckOnClick = true;
+            this.checkedListBoxTasks.Font = new System.Drawing.Font("メイリオ", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.checkedListBoxTasks.FormattingEnabled = true;
+            this.checkedListBoxTasks.Location = new System.Drawing.Point(12, 12);
+            this.checkedListBoxTasks.Name = "checkedListBoxTasks";
+            this.checkedListBoxTasks.Size = new System.Drawing.Size(262, 286);
+            this.checkedListBoxTasks.TabIndex = 0;
+            this.checkedListBoxTasks.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Daily";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.終了ToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(101, 26);
+            // 
+            // 終了ToolStripMenuItem
+            // 
+            this.終了ToolStripMenuItem.Name = "終了ToolStripMenuItem";
+            this.終了ToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.終了ToolStripMenuItem.Text = "終了";
+            this.終了ToolStripMenuItem.Click += new System.EventHandler(this.終了ToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(286, 312);
+            this.Controls.Add(this.checkedListBoxTasks);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Location = new System.Drawing.Point(100, 100);
+            this.Name = "Form1";
+            this.Text = "Daily";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox checkedListBoxTasks;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 終了ToolStripMenuItem;
+
+    }
+}
+
---
+++ Daily/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Daily
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            ShowInTaskbar = false;
+            WindowState = FormWindowState.Minimized;
+            using (StreamReader sr = new StreamReader(Properties.Settings.Default.Tasks))
+            {
+                string lines = sr.ReadToEnd();
+                string nl = Environment.NewLine;
+                string[] liness = lines.Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+                foreach (string line in liness)
+                {
+                    string item = line.Trim();
+                    if (item.Length > 0)
+                    {
+                        checkedListBoxTasks.Items.Add(item);
+                    }
+                }
+            }
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int idx = checkedListBoxTasks.SelectedIndex;
+            string item  = (string)checkedListBoxTasks.Items[idx];
+
+            string output = DateTime.Now + "," + item + "," + checkedListBoxTasks.GetItemChecked(idx);
+            string path = Properties.Settings.Default.LogFile;
+
+            if (!File.Exists(path))
+            {
+                File.CreateText(path);
+            }
+
+            using (StreamWriter sw = File.AppendText(path))
+            {
+                sw.WriteLine(output);
+                Console.WriteLine(output);
+            }
+            
+        }
+
+        private void 終了ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            WindowState = FormWindowState.Normal;
+
+            this.Location = Properties.Settings.Default.AppPosition;
+        }
+
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+           DialogResult result = MessageBox.Show("終了しますか？",
+            "Dailyの終了",
+            MessageBoxButtons.YesNoCancel,
+            MessageBoxIcon.Exclamation,
+            MessageBoxDefaultButton.Button2);
+
+           //何が選択されたか調べる
+           if (result == DialogResult.Yes)
+           {
+               notifyIcon1.Visible = false;
+               Properties.Settings.Default.Save();
+           }
+           else if (result == DialogResult.No)
+           {
+               e.Cancel = true; 
+               WindowState = FormWindowState.Minimized;
+           }
+           else if (result == DialogResult.Cancel)
+           {
+               e.Cancel = true;
+           }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
